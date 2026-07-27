cd31a0e0381809a666c6c239cd282f69458ccfb7
Jiahao Jiang <148902528+ArthurJiang123@users.noreply.github.com>
2026-07-27 13:36:00 -0400

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    // Time: O(log(mn)), Space: O(1)
+    // Binary search the sorted rows. Each each row is sorted [first, last]
+    //  - midRow's last < target → row and above are too small → topRow = midRow+1
+    //  - midRow's first > target → row and below are too large → btmRow = midRow-1
+    //  - else target lies in this row → binary search it.
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int topRow = 0, btmRow = matrix.length - 1;
+        int leftCol = 0, rightCol = matrix[0].length - 1;
+        int midRow = 0;
+
+        while (topRow <= btmRow) {
+            midRow = (topRow + btmRow) / 2;
+            if (matrix[midRow][rightCol] < target) {
+                topRow = midRow + 1;
+            } else if (matrix[midRow][leftCol] > target) {
+                btmRow = midRow - 1;
+            } else {
+                break;
+            }
+        }
+        // topRow > btmRow means the break condition is never reached,
+        // meaning target is outside all the row ranges.
+        if (topRow > btmRow) return false;
+
+        int midCol;
+        while (leftCol <= rightCol) {
+            midCol = (leftCol + rightCol) / 2;
+            if (matrix[midRow][midCol] < target) {
+                leftCol = midCol + 1;
+            } else if (matrix[midRow][midCol] > target) {
+                rightCol = midCol - 1;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
